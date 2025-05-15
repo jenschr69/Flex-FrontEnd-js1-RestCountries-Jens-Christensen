615c3a801f509f3e476386fe7a197ac07afe1b36
Jens Christensen - Glodexa <jens@glodexa.com>
2025-05-15 15:30:49 +0200

    Adding startingpoint

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,64 +1,32 @@
-// Global Variables
-const countriesList = document.getElementById("countries");
-let countries; // will contain "fetched" data
-
-// Event Listeners
-// countriesList.addEventListener("change", event => displayCountryInfo(event.target.value));
-
-countriesList.addEventListener("change", newCountrySelection);
-
-function newCountrySelection(event) {
-  displayCountryInfo(event.target.value);
+// this is an arrow function
+const loadCountryAPI = () =>{
+    // fetch url of rest country from website
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data))
 }
 
-// fetch("https://restcountries.com/v3.1/all")
-// .then(function(res){
-//   // console.log(res);
-//   return res.json();
-// })
-// .then(function(data){
-//   // console.log(data);
-//   initialize(data);
-// })
-// .catch(function(err){
-//   console.log("Error:", err);
-// });
-
-fetch("https://restcountries.com/v3.1/all")
-.then(res => res.json())
-.then(data => initialize(data))
-.catch(err => console.log("Error:", err));
-
-function initialize(countriesData) {
-  countries = countriesData;
-  let options = "";
-  // for(let i=0; i<countries.length; i++) {
-  //   options += `<option value="${countries[i].alpha3Code}">${countries[i].name}</option>`;
-  //   // options += `<option value="${countries[i].alpha3Code}">${countries[i].name} (+${countries[i].callingCodes[0]})</option>`;
-  // }
-  countries.forEach(country => options+=`<option value="${country.alpha3Code}">${country.name}</option>`);
-  // document.getElementById("countries").innerHTML = options;
-  // document.querySelector("#countries").innerHTML = options;
-  countriesList.innerHTML = options;
-  // console.log(countriesList);
-  // console.log(countriesList.value);
-  // console.log(countriesList.length);
-  // console.log(countriesList.selectedIndex);
-  // console.log(countriesList[10]);
-  // console.log(countriesList[10].value);
-  // console.log(countriesList[10].text);
-  countriesList.selectedIndex = Math.floor(Math.random()*countriesList.length);
-  displayCountryInfo(countriesList[countriesList.selectedIndex].value);
+// displaying all countries
+const displayCountries = countries =>{
+    // console.log(countries);
+    const countriesHTML = countries.map(country => getCountry(country));
+    // displaying div to html
+    const container = document.getElementById('countries');
+    container.innerHTML = countriesHTML.join(' ');
 }
 
-function displayCountryInfo(countryByAlpha3Code) {
-  const countryData = countries.find(country => country.alpha3Code === countryByAlpha3Code);
-  // document.querySelector("#flag-container img").src = countryData.flag;
-  // document.querySelector("#flag-container img").alt = `Flag of ${countryData.name}`;  
-  // document.getElementById("capital").innerHTML = countryData.capital;
-  // document.getElementById("dialing-code").innerHTML = `+${countryData.callingCodes[0]}`;
-  // document.getElementById("population").innerHTML = countryData.population.toLocaleString("en-US");
-  // document.getElementById("currencies").innerHTML = countryData.currencies.filter(c => c.name).map(c => `${c.name} (${c.code})`).join(", ");
-  // document.getElementById("region").innerHTML = countryData.region;
-  // document.getElementById("subregion").innerHTML = countryData.subregion;
+// get data and set it to html
+const getCountry = (country) =>{
+    console.log(country)
+    return `
+        <div class="country-div">
+        <img src="${country.flags.png}">
+        <h2>${country.name.common}</h2>
+        <h4>Region: ${country.region}</h4>
+        <h4>Capital: ${country.capital}</h4>
+        <h4>Population: ${country.population}</h4>        
+        </div>
+    `
 }
+// call the funtion to get output in console
+loadCountryAPI()
