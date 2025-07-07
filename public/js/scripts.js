d4bb27e05490b706b00715cb91588d6a3d77927c
Alrik He <alrik_he@hotmail.com>
2025-07-07 11:11:06 +0200

    fork

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,24 +5,52 @@
 // Search by language or country - https://restcountries.com/v3.1/{searchType}/{searchPhrase} ?
 
 // Get all
+let searchType ="" , searchPhrase =""
+
+function validateForm() {
+    let x = document.forms["myForm"]["search"].value;
+    if (x == "") {
+        alert("searchphrase must be filled out");
+        document.getElementById("searchResult").style.display="none"
+        return false;
+    }
+}
+
+let submitButton= document.querySelector("#submit")
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault()
+    document.getElementById("searchResult").style.display="block"
+    validateForm()
+    searchPhrase = document.getElementById("search").value
+    searchType = document.getElementById("searchBy").value
+    console.log(`https://restcountries.com/v3.1/${searchType}/${searchPhrase}`);
+    loadCountryAPI()
+})
+
 const loadCountryAPI = () =>{
     // fetch url of rest country from website
-    fetch('https://restcountries.com/v3.1/all')
+    fetch(`https://restcountries.com/v3.1/${searchType}/${searchPhrase}`)
     .then(res => res.json())
     .then(data => displayCountries(data))
 }
 
 // displaying all countries
-const displayCountries = countries =>{
+const displayCountries = (countries) =>{
+    const container = document.getElementById('countries');
+     if(!Array.isArray(countries)) {container.innerHTML="Nothing found..." ; return }
+
     // console.log(countries);
+    //let countries = []
+
+    //countries.sort( (a,b) => a.name.common < b.name.common)
+
     const countriesHTML = countries.map(country => getCountry(country));
     // displaying div to html
-    const container = document.getElementById('countries');
     container.innerHTML = countriesHTML.join(' ');
 }
 
 // get data and set it to html
-const getCountry = (country) =>{
+function getCountry  (country) {
     console.log(country)
     return `
         <div class="country-div">
@@ -35,4 +63,4 @@ const getCountry = (country) =>{
     `
 }
 // call the funtion to get output in console
-loadCountryAPI()
+//loadCountryAPI()
